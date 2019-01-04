7b3295a4014bec34eafbe9c03df3b761b3f4767d
Matthew Ramsden <ramsden.matthew@gmail.com>
2019-01-03 21:06:09 -0600

    Adding framework and playgrounds

---
+++ RESTLightningFramework/NavigationViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NavigationViewController.swift
+//  RESTLightningFramework
+//
+//  Created by Matthew Ramsden on 1/3/19.
+//  Copyright © 2019 Matthew Ramsden. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let resultSavedGet = Current.lightningSaved.fetchSaved()
+        switch resultSavedGet {
+        case .success(_):
+            let bundle = Bundle(for: ViewNodeViewController.self)
+           let storyboard = UIStoryboard(name: "ViewNodeViewController", bundle: bundle)
+          let vc = storyboard.instantiateViewController(withIdentifier: "ViewNodeViewController") as! ViewNodeViewController
+           DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+               self.navigationController?.pushViewController(vc, animated: true)
+          }
+        case .failure(_):
+            let bundle = Bundle(for: AddNodeViewController.self)
+            let storyboard = UIStoryboard.init(name: "AddNodeViewController", bundle: bundle)
+            let vc = storyboard.instantiateViewController(withIdentifier: "AddNodeViewController") as! AddNodeViewController
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+            
+        }
+        
+    }
+    
+
+}
---
+++ RESTLightningFramework/ViewNodeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewNodeViewController.swift
+//  RESTLightningFramework
+//
+//  Created by Matthew Ramsden on 1/3/19.
+//  Copyright © 2019 Matthew Ramsden. All rights reserved.
+//
+
+import UIKit
+
+class ViewNodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
